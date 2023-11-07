a5affa675330ead1cb722a9a583f162e4d251ae9
Daniela Passos <moitidani@gmail.com>
2023-11-07 13:27:03 -0300

    ~

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,76 @@
 import { Play } from "phosphor-react";
 import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+import { useState } from "react";
+
+const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1, 'Fill in the task'), 
+    minutesAmount: zod.number().min(5, 'You need to focus for at least 5 minutes').max(60, 'You can focus for a max of 60 minutes'),
+})
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
+interface Cycle {
+    id: string;
+    task: string;
+    minutesAmount: number;
+}
 
 export function Home() {
+    const [cycles, setCycles] = useState<Cycle[]>([])
+    const [activeCycleID, setactiveCycleID] = useState<string | null>(null)
+    const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+
+    const { register, handleSubmit, watch, reset } = useForm <NewCycleFormData> ({
+        resolver: zodResolver(newCycleFormValidationSchema),
+        defaultValues: {
+            task: '',
+            minutesAmount: 0
+        }
+    })
+
+    function handleCreateNewCycle(data: NewCycleFormData){
+        const id = String(new Date().getTime())
+
+        const newCycle: Cycle = {
+            id,
+            task: data.task,
+            minutesAmount: data.minutesAmount
+        }
+
+        setCycles( (state) => [...state, newCycle])
+        setactiveCycleID(id)
+
+        reset()
+    }
+
+    const activeCycle = cycles.find((cycle) => cycle.id === activeCycleID)
+
+    const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0 
+    const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
+    const minutesAmount = Math.floor(currentSeconds / 60)
+    const secondsAmount = currentSeconds % 60
+
+    const minutes = String(minutesAmount).padStart(2, '0')
+    const seconds = String(secondsAmount).padStart(2, '0')
+
+    const task = watch('task')
+    const isSubmitDisabled = !task;
+
     return (
         <HomeContainer>
-            <form action="">
+            <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
                 <FormContainer>
                     <label htmlFor="task">I am working on</label>
-                    <TaskInput id="task" placeholder="Give your project a name" />
+
+                    <TaskInput 
+                        id="task" 
+                        list="task-suggestions"
+                        placeholder="Give your project a name"
+                        {...register('task')}
+                        />
                     <datalist id="task-suggestions">
                         <option value="Project 1"/>
                         <option value="Project 2"/>
@@ -22,20 +85,21 @@ export function Home() {
                         step={5}
                         min={5}
                         max={60}
+                        {...register('minutesAmount', {valueAsNumber: true})}
                     />
                     <span>minutes.</span>
                 </FormContainer>
 
                 <CountdownContainer>
-                    <span>0</span>
-                    <span>0</span>
+                    <span>{minutes[0]}</span>
+                    <span>{minutes[1]}</span>
                     <Separator>:</Separator>
-                    <span>0</span>
-                    <span>0</span>
+                    <span>{seconds[0]}</span>
+                    <span>{seconds[1]}</span>
                 </CountdownContainer>
 
 
-                <StartCountdownButton disabled type="submit">
+                <StartCountdownButton disabled={isSubmitDisabled} type="submit">
                     <Play size={24} />
                     Start
                 </StartCountdownButton>
