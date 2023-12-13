c58dddec2747dd48dcf13e0e040c6faed159c33c
Daniela Passos <moitidani@gmail.com>
2023-12-13 08:46:38 -0300

    ~

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,47 +1,55 @@
-import { HistoryContainer, HistoryList, Status } from "./styles";
+import { useContext } from 'react'
+import { formatDistanceToNow } from 'date-fns'
+import { CyclesContext } from '../Home'
+import { HistoryContainer, HistoryList, Status } from './styles'
 
-export function History(){
-    return(
-        <HistoryContainer>
-            <h1>My history</h1>
-            <HistoryList>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Task</th>
-                            <th>Duration</th>
-                            <th>Start</th>
-                            <th>End</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Task</td>
-                            <td>20 minutes</td>
-                            <td>2 months ago</td>
-                            <td>
-                                <Status statusColor='green'>Done</Status>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Task</td>
-                            <td>20 minutes</td>
-                            <td>2 months ago</td>
-                            <td>
-                                <Status statusColor='yellow'>In progress</Status>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>Task</td>
-                            <td>20 minutes</td>
-                            <td>2 months ago</td>
-                            <td>
-                                <Status statusColor='red'>Blocked</Status>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </HistoryList>
-        </HistoryContainer>
-    )
+export function History() {
+  const { cycles } = useContext(CyclesContext)
+
+  return (
+    <HistoryContainer>
+      <h1>My history</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Task</th>
+              <th>Duration</th>
+              <th>Start</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cycles.map((cycle) => {
+              return (
+                <tr key={cycle.id}>
+                  <td>{cycle.task}</td>
+                  <td>{cycle.minutesAmount} minutes</td>
+                  <td>
+                    {formatDistanceToNow(cycle.startDate, {
+                      addSuffix: true,
+                    })}
+                  </td>
+                  <td>
+                    {cycle.finishedDate && (
+                      <Status statusColor="green">Done</Status>
+                    )}
+
+                    {cycle.interruptedDate && (
+                      <Status statusColor="red">Interrupted</Status>
+                    )}
+
+                    {!cycle.finishedDate && !cycle.interruptedDate && (
+                      <Status statusColor="yellow">In progress</Status>
+                    )}
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
 }
